4d8f89178d0a86de2eb657bf803ef3aba59db39c
Kevin Vecmanis <vanaurum@vanurum-master.local>
2019-05-27 09:23:29 -0400

    ..

--- examples/sample_from_sklearn.py
+++ examples/sample_from_sklearn.py
@@ -3,7 +3,7 @@ from mlautomator.mlautomator import MLAutomator
 
 if __name__=='__main__':
     x,y=from_sklearn('wine')
-    automator=MLAutomator(x,y,iterations=30,algo_type='classifier',score_metric='neg_log_loss')
+    automator=MLAutomator(x,y,iterations=3,algo_type='classifier',score_metric='neg_log_loss')
     automator.find_best_algorithm()
     automator.print_best_space()
     
